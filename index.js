b1298746a7c8f88f0e83b25b7008eee6327a8a01
leonwong03 <108467768+leonwong03@users.noreply.github.com>
2022-08-08 07:07:36 -0400

    commit tree baf63ab2832aae6aee39113ce75e4d586bed684e

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31
+const num2 = 31
 
-// Write your code here
+let multiply = num1 + num2
+    
+const random = Math.floor(Math.random() * 10 + 1)
+
+const num3 = 19
+const num4 = 5
+let mod = num3 % num4
+
+const max = Math.max(0, 5, 10, 15, 20)
